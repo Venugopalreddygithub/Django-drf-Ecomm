485b9ad3fc25b8d0ddd2bd7551f41acff4acb170
venugopal <bvenugopalreddy16@gmail.com>
2024-02-21 22:40:11 +0530

    create tag api is created

---
+++ tags/serializer.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers 
+from tags.models import Tags 
+
+class WriteTagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model= Tags 
+        fields = ('name',)
+        
+
+class ReadTagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model= Tags 
+        fields = ("id", "name", "slug", "created_at", "updated_at")
